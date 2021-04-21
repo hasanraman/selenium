75a096103b6606926b54b6b40a43184d0dc11f7a
hasanraman <79028159+hasanraman@users.noreply.github.com>
2021-04-20 21:45:33 -0400

    configuration

---
+++ src/test/java/testngday7/Class3.java
@@ -0,0 +1,39 @@
+package testngday7;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Class3 {
+    @Test
+    public void TC2_configurationProperties() throws FileNotFoundException {
+        //to be able to make java understand my configuration file i need to cennect and then open the file
+        Properties properties=new Properties();// allows us to read .properties file
+
+        String pathForProperties="configuration.properties";
+        // need to get path from file it can be absulate path or root path
+//        /Users/sevginhekimoglu/IdeaProjects/seleniumTestNG/configuration.properties
+
+        // we need to tell compiler to open our file
+        // fileinputstream method will throw an exception (FileNotFoundException)
+        // you need to handle with try catch or throws keyword
+        FileInputStream fileInputStream=new FileInputStream(pathForProperties);
+
+        try {
+            properties.load(fileInputStream); // file needs to be loaded in the automation
+            // we use load method that comes from properties
+            //this throw an exaption  IOException
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //time to read file
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+        System.out.println("properties.getProperty(\"SmartBear_url\") = " + properties.getProperty("SmartBear_url"));
+        System.out.println("properties.getProperty(\"SmartBear_username\") = " + properties.getProperty("SmartBear_username"));
+
+    }
+}
